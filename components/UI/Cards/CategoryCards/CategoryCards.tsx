890e693fa31ae3976d6b064fe8d87db1f07a0b52
Zair <nurmukhamadov.z@future-group.ru>
2022-05-10 10:57:38 +0300

    .

--- components/UI/Cards/CategoryCards/CategoryCards.tsx
+++ components/UI/Cards/CategoryCards/CategoryCards.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import classNames from "classnames";
 
 const CategoryCards: React.FC = () => {
@@ -7,42 +8,19 @@ const CategoryCards: React.FC = () => {
       "grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6",
       "gap-4 justify-items-center text-center",
     )}>
-      <li>
-        <div className="p-2 md:p-6 rounded-lg bg-base-100 mb-4">
-          <img src="https://api.lorem.space/image/watch?w=120&h=120"/>
-        </div>
-        <p>Категория</p>
-      </li>
-      <li>
-        <div className="p-2 md:p-6 rounded-lg bg-base-100 mb-4">
-          <img src="https://api.lorem.space/image/watch?w=120&h=120"/>
-        </div>
-        <p>Категория</p>
-      </li>
-      <li>
-        <div className="p-2 md:p-6 rounded-lg bg-base-100 mb-4">
-          <img src="https://api.lorem.space/image/watch?w=120&h=120"/>
-        </div>
-        <p>Категория</p>
-      </li>
-      <li>
-        <div className="p-2 md:p-6 rounded-lg bg-base-100 mb-4">
-          <img src="https://api.lorem.space/image/watch?w=120&h=120"/>
-        </div>
-        <p>Категория</p>
-      </li>
-      <li>
-        <div className="p-2 md:p-6 rounded-lg bg-base-100 mb-4">
-          <img src="https://api.lorem.space/image/watch?w=120&h=120"/>
-        </div>
-        <p>Категория</p>
-      </li>
-      <li>
-        <div className="p-2 md:p-6 rounded-lg bg-base-100 mb-4">
-          <img src="https://api.lorem.space/image/watch?w=120&h=120"/>
-        </div>
-        <p>Категория</p>
-      </li>
+
+      {[1,2,3,4,5,6].map(item =>
+        <li key={item}>
+          <Link href='/'>
+            <a>
+              <div className="p-2 md:p-6 rounded-lg bg-base-100 mb-4">
+                <img src="https://api.lorem.space/image/watch?w=120&h=120"/>
+              </div>
+              <p className="text-sm md:text-md">Категория</p>
+            </a>
+          </Link>
+        </li>
+      )}
     </ul>
   );
 };
