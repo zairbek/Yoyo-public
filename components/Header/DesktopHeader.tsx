024d7a42cc5f0b4e5f9613d53ca76379b70044c2
Zair <nurmukhamadov.z@future-group.ru>
2022-12-04 20:42:33 +0600

    .

--- components/Header/DesktopHeader.tsx
+++ components/Header/DesktopHeader.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import HeaderSearch from "../UI/Search/HeaderSearch/HeaderSearch";
 import BasketButtonDropdown from "../UI/Basket/BasketIcon/BasketButtonDropdown";
 import AccountButtonCircle from "../UI/Account/AccountButtonCircle/AccountButtonCircle";
+import {Routes} from "../../utils/routes";
 
 const DesktopHeader = () => {
   return (
@@ -32,14 +33,16 @@ const DesktopHeader = () => {
 
         <div className="gap-x-2">
           {/*lang*/}
-          {/*
-          выбор языка интерфейса
-          <LanguageButton/>*/}
+
+          {/*выбор языка интерфейса*/}
+          {/*<LanguageButton/>*/}
 
           {/*favorite*/}
-          <button className="btn btn-circle btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/></svg>
-          </button>
+          <Link href={Routes.favorite}>
+            <a href={Routes.favorite} className="btn btn-circle btn-ghost">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/></svg>
+            </a>
+          </Link>
 
           {/*basket*/}
           <BasketButtonDropdown/>
