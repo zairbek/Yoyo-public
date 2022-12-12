ac5c543e728b2ad83e0998b4369c2e725d0d3f70
Zair <nurmukhamadov.z@future-group.ru>
2022-12-12 23:32:14 +0600

    new icons

---
+++ components/shared/Icon/Icons/Outline/ArchiveBox.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {IconProps, IconSizes} from "../../index";
+import cx from "classnames";
+
+const ArchiveBox: React.FC<IconProps> = ({
+                                           size = IconSizes.medium
+                                         }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+         className={cx(size)}>
+      <path strokeLinecap="round" strokeLinejoin="round"
+            d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"/>
+    </svg>
+  );
+};
+
+export default ArchiveBox;
