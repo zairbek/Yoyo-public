3a7675b60ca552575d0c308f0044c0df533fcdd4
Zair <nurmukhamadov.z@future-group.ru>
2022-12-15 22:25:00 +0600

    fix

--- components/features/indexPage/catalogs/TopVerticalCatalogs/TopVerticalCatalogs.tsx
+++ components/features/indexPage/catalogs/TopVerticalCatalogs/TopVerticalCatalogs.tsx
@@ -4,6 +4,7 @@ import {Button} from "../../../../shared/Button";
 import {ButtonColors, ButtonSize} from "../../../../shared/Button/Button";
 import Heart from "../../../../shared/Icon/Icons/Outline/Heart";
 import {data} from "../../../../../mocks/catalogs.mock";
+import cx from "classnames";
 
 interface TopVerticalCatalogsProps {
   isMobile?: boolean,
@@ -13,7 +14,10 @@ const TopVerticalCatalogs: React.FC<TopVerticalCatalogsProps> = ({
   isMobile,
 }) => {
   return (
-    <Section isMobile={isMobile} direction={SectionDirections.Row} className="my-6 overflow-y-auto">
+    <Section isMobile={isMobile} direction={SectionDirections.Row} className={cx(
+      "overflow-y-auto",
+      isMobile ? 'py-2 mt-6 mb-2' : 'py-3 my-3'
+    )}>
       {data.map((catalog, key) => (
         <Button key={key} size={ButtonSize.sm} beforeIcon={<Heart/>} color={ButtonColors.ghost}>{catalog.name}</Button>
       ))}
